0cc1f2572342dc664147d77fe90b8920225b9d5f
Lucas Silva <lucass.uchiha2002@gmail.com>
2025-11-21 17:25:32 -0300

    Add files via upload

---
+++ drizzle/0001_strange_hemingway.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `products` ADD `sizes` text;--> statement-breakpoint
+ALTER TABLE `products` ADD `colors` text;--> statement-breakpoint
+ALTER TABLE `products` ADD `tags` text;--> statement-breakpoint
+ALTER TABLE `products` ADD `original_price` text;
